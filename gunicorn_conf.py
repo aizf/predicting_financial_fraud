dc287d95200dac039a39ef3141872a8d2ce0b768
ubuntu <ubuntu@localhost.localdomain>
2020-02-26 22:29:10 +0800

    1

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -8,6 +8,8 @@ bind = '0.0.0.0:5000'
 chdir = '/home/ubuntu/GitHub/predicting_financial_fraud'
 workers = multiprocessing.cpu_count() * 2 + 1    #进程数
 threads = 2 #指定每个进程开启的线程数
+daemon = True
+
 pidfile = 'log/gunicorn.pid'
 accesslog = "log/gunicorn_access.log"      #访问日志文件
 errorlog = "log/gunicorn_error.log"        #错误日志文件
