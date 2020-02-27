b5e4ce684aa3a0598cd4b0361666638dd066cf21
aizf <45379838+aizf@users.noreply.github.com>
2020-02-28 04:47:18 +0800

    fix bug

--- templates/changePath.py
+++ templates/changePath.py
@@ -1,26 +1,30 @@
-from pyquery import PyQuery as pq
-
+import re
 filePath = "./templates/index.html"
 
-doc = pq(filename=filePath)
+with open(filePath, "r") as f:
+    content = f.read()
 
-# print(doc)
+# content = """    <link href=/assets/css/app.9c1d24ed.css rel=preload as=style>
+#     <link href=/assets/css/chunk-vendors.2e31bbc2.css rel=preload as=style>
+#     <link href=/assets/js/app.0a10128a.js rel=preload as=script>
+#     <link href=/assets/js/chunk-vendors.0195ce98.js rel=preload as=script>
+#     <link href=/assets/css/chunk-vendors.2e31bbc2.css rel=stylesheet>
+#     <script src=/assets/js/chunk-vendors.0195ce98.js></script>
+#     <script src=/assets/js/app.0a10128a.js></script>"""
+
+pattern = re.compile(r'(href=|src=)(/)(assets/.*?\.(?:js|css))')
+
+# print(pattern.findall(content))
 
 
-def changePath(string):
-    return "{{{{url_for('static',filename='{0}')}}}}".format(string[1:])
+def fun(s):
+    group1 = s.group(1)
+    # group2 = s.group(2)
+    group3 = s.group(3)
+    return group1 + "\"" + r"{{url_for('static',filename='" + group3 + r"')}}" + "\""
 
 
-links = doc("head link")
-for link in links.items():
-    href = link.attr["href"]
-    link.attr["href"] = changePath(href)
-
-scripts = doc("body script")
-for script in scripts.items():
-    src = script.attr["src"]
-    script.attr["src"] = changePath(src)
+fixed_content = re.sub(pattern, fun, content)
 
 with open(filePath, "w") as f:
-    f.write(str(doc))
-# print(doc.html(method='html'))
+    f.write(fixed_content)
